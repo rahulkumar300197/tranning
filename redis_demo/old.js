94c56af27f71379829e5e87b97e918541ae16764
rahulkumar300197 <rahulkumar310197@rediffmail.com>
2017-11-27 16:15:40 +0530

    final 41527

--- redis_demo/old.js
+++ redis_demo/old.js
@@ -39,12 +39,12 @@ server.register(require('inert'), (err) => {
                 }
             },
             handler: async function (request, reply) {
-                var data=request.payload.name;                           
-                console.log(data);
-               //// var usr_data = new array();
-                //var usr_data=  await getData(data);
-                
-           
+              var data=request.payload.name;
+              getData(data).then(result=>{  
+                if(result.length!=0){ 
+                  reply(result);
+                }
+              });     
             }
         });    
 
@@ -61,60 +61,67 @@ server.start(function (err) {
 });
 
 function db_getData(id){
-    var data= new Array;
-    MongoClient.connect(url, function(err, db) {
+    return new Promise(function(resolve, reject) {
+      var data= new Array;
+      MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         db.collection('emp_test').find({name:id}).toArray(function(err, result) {
           if (err) throw err;
-          console.log("Searching DataBase");
-          return setData(result,id);
-          
+          console.log("Searching DataBase");  
+         // console.log(result); 
+          if(result.length != 0){
+            //console.log("1");  
+            resolve(result); 
+          } 
+          else{
+           // console.log("2");  
+            reject(result);
+          }       
         });
-    }); 
-    return data;
+      }); 
+      
+    })  
 
 }
 
-function getData(id)
-{   console.log(id);
-    var data= [];
+function getData(id){
+  return new Promise(function(resolve, reject) { 
+     //console.log(id);
+     var data= [];
 
-    client.lrange(id, 0, -1, function (error, items) {
-        if (error) throw error;
+     client.lrange(id, 0, -1, function (error, items) {
+       if (error) throw error;
       //  console.log("1");
-        else if(items.length != 0 ) {
-            console.log("Data from rediss");
-            var x=items;
-            console.log(x);
-            //console.log(typeof items[0]);
-
-            return x;
-            
-        }
-        else return db_getData(id);        
-           
-         
-    });
+       else if(items.length != 0 ) {
+         console.log("Data from rediss");
+         resolve(items);     
+       }
+       else db_getData(id).then(result=>{  
+         if(result.length!=0){ 
+         //  console.log("3"); 
+           setData(result,id).then(result=>{resolve(result);}).catch(err=>{
+             throw err;
+           })
+         }
+       });        
+     
+     });
+   })
 }
 
 
-function setData(data,dname)
-{ if(data[0] == null){
-    console.log("Not found user "+dname);
-    return ;
-  } 
-  //console.log("dvd");
-  console.log(data);
-  // dname.toLower(); 
-  console.log(dname);
-  // client.(dname,data);
+function setData(data,dname){
+   // console.log("4");  
+  return new Promise(function(resolve, reject) {
+    if(data[0] == null){
+      console.log("Not found user "+dname);
+     // console.log("5");
+      reject(data);
+    } 
     data.forEach(function (item){
-     client.rpush(dname, JSON.stringify(item)
-        , function(err, reply){
-         if(err) console.log(err);
-         console.log("Data added to redis");
-         return  data;
-     });
-   });
-     
+      client.rpush(dname, JSON.stringify(item));
+    });
+  //  console.log("6");
+    resolve(data);    
+  })
 }
